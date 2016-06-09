4d5d3727591ddedc9ba999be8280cbc11abd30c3
mattleon <matthieu.leon@etu.unistra.fr>
2016-06-09 11:52:32 +0200

    Modificiation Datasets, amélioration lecture données

--- linear_regression/lr.py
+++ linear_regression/lr.py
@@ -3,23 +3,25 @@ import numpy
 import matplotlib.pyplot as plt
 rng = numpy.random
 
-#Paramètres
+#Parametres
 #fera Y en fonction de X
 # 0 = time; 1 = cwnd; 2 = rtt; 3 = dupack; 4 = retransmission; 5 = drops
-axe_X = 1
-axe_Y = 5
+axe_X = 0
+axe_Y = 1
 learning_rate = 0.01
 training_epochs = 1000
 display_step = 50
 
-# Training Data
+# Training Data & Test Data
 fich = open('cwnd_reno.dat','r')
 val = fich.read()
 tmp = val.split('\r\n')
-new_liste = list()
 train_x = list()
 train_y = list()
-i = 1
+test_x = list()
+test_y = list()
+
+i = j = 1
 while i < len(tmp)-1:
     tmp1 = tmp[i].split(';')
     train_x.append(float(tmp1[axe_X]))
@@ -29,31 +31,24 @@ while i < len(tmp)-1:
     #liste_vide.append(mon_tuple)
     i += 150
 
+while j < len(tmp)-1:
+    tmp1 = tmp[j].split(';')
+    test_x.append(tmp1[axe_X])
+    test_y.append(tmp1[axe_Y])
+    #mon_tuple = [float(tmp1[0]), float(tmp1[1])]
+    #time & cwnd
+    #liste_vide.append(mon_tuple)
+    j += 10
+
 fich.close()
 train_X = numpy.asarray(train_x)
 train_Y = numpy.asarray(train_y)
 
 n_samples = train_X.shape[0]
 
-# Test data
-fich = open('cwnd_reno.dat','r')
-val = fich.read()
-tmp = val.split('\r\n')
-liste_vide = list()
-test_x = list()
-test_y = list()
-i = 1
-while i < len(tmp)-1:
-    tmp1 = tmp[i].split(';')
-    test_x.append(tmp1[axe_X])
-    test_y.append(tmp1[axe_Y])
-    #mon_tuple = [float(tmp1[0]), float(tmp1[1])]
-    #time & cwnd
-    #liste_vide.append(mon_tuple)
-    i += 10    fich.close()
-
 test_X = numpy.asarray(test_x)
 test_Y = numpy.asarray(test_y)
+
 # tf Graph Input
 X = tf.placeholder("float")
 Y = tf.placeholder("float")

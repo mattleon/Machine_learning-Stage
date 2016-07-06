f1896768f2ad5c5f183036c25ab3ac7949c5d14d
mattleon <matthieu.leon@etu.unistra.fr>
2016-07-06 16:44:58 +0200

    Petits tests pour retrouver valeurs labels - fonctionne

--- dec_tree.py
+++ dec_tree.py
@@ -7,6 +7,7 @@ from sklearn.tree import DecisionTreeClassifier
 n_classes = 3
 plot_colors = "byr"
 plot_step = 0.02
+plot_step = 1
 label_names = ['0', '1', '2']
 
 # On charge les donnees
@@ -14,7 +15,7 @@ df = pd.read_csv('cwnd.dat', delimiter = ';')
 x = []
 y = []
 dp= pd.read_csv('label.csv')
-for i in range(0,len(df)/2):
+for i in range(0,len(df)/3):
     x.append([df['time'].values[i], df['cwnd'].values[i]])
     y.append(dp['label'].values[i])
 y = np.array(y)
@@ -24,14 +25,20 @@ X = np.array(x)
 clf = DecisionTreeClassifier().fit(X, y)
 
 #On choisit les limites en fonction des valeurs dans les ensembles
-x_min, x_max = 10, X[:, 0].max() + 1
+x_min, x_max = X[:, 0].min() - 1, X[:, 0].max() + 1
 y_min, y_max = 0, X[:, 1].max() + 1
 xx, yy = np.meshgrid(np.arange(x_min, x_max, plot_step),
                      np.arange(y_min, y_max, plot_step))
 #Prediction -> l'algo essaye de predire quel point appartient a quelle classe
 Z = clf.predict(np.c_[xx.ravel(), yy.ravel()])
-
 Z = Z.reshape(xx.shape)
+"""
+Z est un tableau de tableau tq : Z[y][X] = label, avec un ecart
+de plot_step
+"""
+
+#print(Z[51][1])
+
 #contour -> met les labels predits (avec les couleurs) sur le graphe
 cs = plt.contourf(xx, yy, Z)
 
@@ -49,4 +56,4 @@ plt.axis("tight")
 
 plt.suptitle("Arbre de decision")
 plt.legend()
-#plt.show()
+plt.show()
